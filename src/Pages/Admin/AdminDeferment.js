7f93375f43993558b3c6f421d7a035377f32fb8e
SelasiMadeItHere <selasiprecious20@gmail.com>
2023-05-10 04:27:33 +0000

    10/05/2023-04:2023am

--- src/Pages/Admin/AdminDeferment.js
+++ src/Pages/Admin/AdminDeferment.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Navbar from '../../components/Navbar'
 import Lpane from '../../components/Lpane'
-import { Card, Table, TableRow, TableCell, TableHead, TableBody, Button, Stack } from '@mui/material'
+import { Card, Table, TableRow, TableCell, TableHead, TableBody, Button, Stack} from '@mui/material'
 
 function AdminDeferment() {
     const [data, setData] = useState([]);
@@ -14,35 +14,36 @@ function AdminDeferment() {
         loadData();
     }, []);
     return (
-        <div className='bg-indigo-100 grid grid-cols-5 h-full min-h-screen'>
+        <div className='bg-indigo-100 grid grid-cols-9 h-screen pb-12 min-h-screen'>
             <div>
                 <Navbar />
-                {/* <Lpane className='col-span-2' /> */}
+                <Lpane className='col-span-2' />
             </div>
 
-            <div className='grid col-span-4 w-[100%] col-start-2 col-end-4 mt-28'>
+            <div className='grid col-start-3 col-span-9 col-end-9 mt-28'>
                 <Card>
-                    <h1 className=' text-2xl font-semibold text-center bg-sky-800 text-white p-6'>Deferment Requests</h1>
-                    <Table className='mt-5'>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell style={{ fontWeight: "bolder" }}>ID NO.</TableCell>
+                    <Table>
+                        <TableHead className='bg-gray-900' style={{ color: 'white' }}>
+                            <TableRow className=''>
+                                <TableCell></TableCell>
+                                <TableCell  >ID NO.</TableCell>
                                 <TableCell style={{ fontWeight: "bolder" }}>LEVEL</TableCell>
-                                <TableCell style={{ fontWeight: "bolder" }}>SEMESTER</TableCell>
-                                <TableCell style={{ fontWeight: "bolder" }}>SURNAME</TableCell>
-                                <TableCell style={{ fontWeight: "bolder" }}>PROGRAM</TableCell>
-                                <TableCell style={{ fontWeight: "bolder" }}>LEVEL</TableCell>
-                                <TableCell style={{ fontWeight: "bolder" }}>CAMPUS</TableCell>
-                                <TableCell style={{ fontWeight: "bolder" }}>STATUS</TableCell>
+                                <TableCell style={{ fontWeight: "bolder" }}>CURRENT SEMESTER</TableCell>
+                                <TableCell style={{ fontWeight: "bolder" }}>DEFERMENT YEAR</TableCell>
+                                <TableCell style={{ fontWeight: "bolder" }}>RETURNING YEAR</TableCell>
+                                <TableCell style={{ fontWeight: "bolder" }}>PREVIOUS DEFERMENT</TableCell>
+                                <TableCell style={{ fontWeight: "bolder" }}>REASON</TableCell>
+                                <TableCell style={{ fontWeight: "bolder" }}>DATE</TableCell>
+                                <TableCell style={{ fontWeight: "bolder" }}>DEFERMENT ID</TableCell>
                                 <TableCell style={{ fontWeight: "bolder" }}>ACTION</TableCell>
                             </TableRow>
                         </TableHead>
 
-                        <TableBody>
+                        <TableBody className='text-sm'>
                             {data.map((item, index) => {
                                 return (
-                                    <tr key={item.id}>
-                                        {/* <th scope="row">  {index + 1}</th> */}
+                                    <tr key={item.id} className=' border '>
+                                        <th scope="row">  {index + 1}</th>
                                         <td className=' text-center p-3'>{item.stuid}</td>
                                         <td className=' text-center p-3'>{item.clevel}</td>
                                         <td className=' text-center p-3'>{item.csem}</td>
