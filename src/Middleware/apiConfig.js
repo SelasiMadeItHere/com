41f61dea1b81c2dcb0e28ecb4d1a84dd6e7eaaea
SelasiMadeItHere <selasiprecious20@gmail.com>
2024-04-23 10:07:34 +0000

    updating the middleware

--- src/Middleware/apiConfig.js
+++ src/Middleware/apiConfig.js
@@ -1,6 +1,6 @@
 const config = {
   // backendUrl: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5002'
-  backendUrl: process.env.REACT_APP_BACKEND_URL || 'https://com-server-7rtz.onrender.com/'
+  backendUrl: process.env.REACT_APP_BACKEND_URL || 'https://com-server-7rtz.onrender.com'
 };
 
   export default config;
